cbf4c206a44b95080425dc6e8430590a74c7342d
chuckpeplinski <chuck.peplinski@i3pd.com>
2024-03-26 14:17:41 -0500

    v3.7.1, 676 merged

--- include/app_version.h
+++ include/app_version.h
@@ -7,8 +7,8 @@
 #define _APP_VERSION_H_
 
 #define APP_MAJOR_VERSION 3
-#define APP_MINOR_VERSION 6
-#define APP_PATCH_VERSION 1
+#define APP_MINOR_VERSION 7
+#define APP_PATCH_VERSION 0
 
 // appends a note string to the version
 #define DEV_BUILD
