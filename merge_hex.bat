be02e6d77a00e398e25048bee55d923be3b54371
chuckpeplinski <chuck.peplinski@i3pd.com>
2024-04-18 09:54:49 -0500

    v4.0.6: Removed obsolete messages_per_ack used in file transfers.

--- merge_hex.bat
+++ merge_hex.bat
@@ -7,13 +7,14 @@ set APP=reach-silabs.s37
 
 echo Create combined hex file reach-silabs-combined.hex
 @echo on
-%PATH_SCMD%\commander.exe convert %BOOTLOADER% %APP% -o reach-silabs-combined.hex
+%PATH_SCMD%\commander.exe convert %BOOTLOADER% %APP% -o reach-silabs-combined_v.hex
 
 
 set PATH_GCCARM=C:\SiliconLabs\SimplicityStudio\v5\developer\toolchains\gnu_arm\12.2.rel1_2023.7
 
 echo call create_bl_files.bat to created .gbl files for FW update.
 call ..\create_bl_files.bat
+mv 
 
 rem force rebuild of dated banner
 rm src/cli.o
